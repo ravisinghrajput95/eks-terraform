2c942e2fddbeb3eabb13133880a94ef2dff44fc5
Ravi Singh Rajput <92205137+ravisinghrajput95@users.noreply.github.com>
2025-07-14 17:55:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ module "eks" {
   vpc_cidr           = var.vpc_cidr
   aws_account_id     = var.aws_account_id
 }
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
